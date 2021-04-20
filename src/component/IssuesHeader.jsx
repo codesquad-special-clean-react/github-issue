39efde831a5c115cd8c5f5d4d0407b36c5a2776e
남윤서 <devysnam@gmail.com>
2021-04-21 00:50:10 +0900

    Add icon in IssuesHeader

--- src/component/IssuesHeader.jsx
+++ src/component/IssuesHeader.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import styled from 'styled-components'
-
-const Header = styled.div`
-  padding: 2em;
-  text-align: center;
-  color: white;
-  background: #242A2E;
-`;
+import {GoRepo} from "react-icons/all";
 
 const IssuesHeader = ({ title = "ISSUES" }) => {
   return (
       <div>
-        <Header>{title}</Header>
+        <Header>
+          <GoRepo color={"#AAA"}/>
+          <Title>{title}</Title>
+        </Header>
       </div>
   );
 };
+
+const Header = styled.div`
+  padding: 2em;
+  text-align: center;
+  background: #242A2E;
+`;
+
+const Title = styled.span`
+  padding: 0.3em;
+  color: white;
+`
 
 export default IssuesHeader;
