78f622889be349b922a8824af09b04c46d415a92
남윤서 <devysnam@gmail.com>
2021-04-21 00:42:29 +0900

    Change Header using styled-components

--- src/component/IssuesContents.jsx
+++ src/component/IssuesContents.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 
-const IssuesContents = ({ title = "ISSUES" }) => {
+const IssuesContents = () => {
   return (
       <div>
-        <span>{title}</span>
       </div>
   );
 };
--- src/component/IssuesHeader.jsx
+++ src/component/IssuesHeader.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import styled from 'styled-components'
 
-const IssuesHeader = () => {
+const Header = styled.div`
+  padding: 2em;
+  text-align: center;
+  color: white;
+  background: #242A2E;
+`;
+
+const IssuesHeader = ({ title = "ISSUES" }) => {
   return (
       <div>
-
+        <Header>{title}</Header>
       </div>
   );
 };
