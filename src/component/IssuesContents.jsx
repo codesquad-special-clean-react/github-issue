0312928cf26c6e284a16348aff8b843f649327cf
남윤서 <devysnam@gmail.com>
2021-04-21 01:13:44 +0900

    Add Labels & Milestones button

--- src/component/IssuesContents.jsx
+++ src/component/IssuesContents.jsx
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styled from 'styled-components'
 
 const IssuesContents = () => {
+  const [isActiveLabels, setIsActiveLabels] = useState(false)
+  const [isActiveMilestones, setIsActiveMilestones] = useState(false)
+
+  const onClickLabels = () => {
+    setIsActiveLabels(() => true)
+    setIsActiveMilestones(() => false)
+  }
+
+  const onClickMilestones = () => {
+    setIsActiveMilestones(() => true)
+    setIsActiveLabels(() => false)
+  }
+
   return (
-      <div>
-      </div>
+      <Container>
+        <TabButton active={isActiveLabels} onClick={() => onClickLabels()}>Labels</TabButton>
+        <TabButton active={isActiveMilestones} onClick={() => onClickMilestones()}>Milestones</TabButton>
+        <CreateButton>New label</CreateButton>
+      </Container>
   );
 };
+
+const Container = styled.section`
+padding: 3em 10em;
+`
+
+const TabButton = styled.button`
+padding: 1em;
+border: 1px solid;
+background-color: ${props => props.active ? "#1265D6" : "white"};
+color: ${props => props.active ? "white" : "black"};
+margin-right: 1em;
+`
+
+const CreateButton = styled.button`
+padding: 1em;
+border: none;
+background-color: #2CB44B;
+color: white;
+position: absolute;
+right: 10em;
+`
 
 export default IssuesContents;
