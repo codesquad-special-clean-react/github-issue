170325348e95b1f4c7d88120e5d24a30eb7c841f
ttumzzi <ssumini0829@gmail.com>
2021-04-22 20:55:26 +0900

    기본 셋팅

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import TabContainer from './components/TabContainer';
+import { tabs } from './utils/constant';
 
 function App() {
+  const [selectedTab, selectTab] = useState(tabs.label);
+  const [isNewLabelEditorOpened, setNewLabelEditorOpen] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="head">
+        <TabContainer selectedTab={selectedTab} />
+      </div>
     </div>
   );
 }
