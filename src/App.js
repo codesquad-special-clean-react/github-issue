678d62be7efcae7955e5b9f8f42211ee25f03b39
domuk-k <dannyworks102@gmail.com>
2021-04-23 12:48:42 +0900

    init: root component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import './App.css';
-import { NewLabelButton } from './components/Button';
+import { Labels, Milestones } from 'pages';
+import GlobalStyle from 'theme/globalStyle';
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import { ThemeProvider } from 'theme/theme';
+import Layout from 'components/Layout';
 
-function App() {
-  return <NewLabelButton />;
-}
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider>
+        <Router>
+          <Layout>
+            <Switch>
+              <Route path="/milestones" component={Milestones}></Route>
+              <Route path="/labels" component={Labels}></Route>
+              <Redirect to="/labels" />
+            </Switch>
+          </Layout>
+        </Router>
+      </ThemeProvider>
+    </>
+  );
+};
 
 export default App;
