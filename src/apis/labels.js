1586c00b2b57eb5bd4518d325bb789b9664c644e
parkyes90 <parkyes90@gmail.com>
2021-04-23 23:14:41 +0900

    feat: Add label 생성 API, 예외 처리

--- src/apis/labels.js
+++ src/apis/labels.js
@@ -1,6 +1,25 @@
 const LABELS_URL = "http://localhost:3001/labels";
 
 export const fetchLabels = async () => {
-  const response = await fetch(LABELS_URL);
-  return response.json();
+  try {
+    const response = await fetch(LABELS_URL);
+    return response.json();
+  } catch (e) {
+    alert(e);
+  }
+};
+
+export const postLabel = async (params) => {
+  try {
+    const response = await fetch(LABELS_URL, {
+      method: "POST",
+      body: JSON.stringify(params),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return response.json();
+  } catch (e) {
+    alert(e);
+  }
 };
