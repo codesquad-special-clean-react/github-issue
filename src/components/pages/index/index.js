0b7339547ca0b8053961a1bc8eccc54b7b5c1391
parkyes90 <parkyes90@gmail.com>
2021-04-23 22:56:51 +0900

    chore: api 호출 컴포넌트 변경

--- src/components/pages/index/index.js
+++ src/components/pages/index/index.js
@@ -1,8 +1,7 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import styles from "./index.module.css";
 import Header from "../../layouts/header";
 import Main from "../../layouts/main";
-import { fetchLabels } from "../../../apis/labels";
 import Toolbar from "../../organisms/toolbar";
 import { TABS } from "../../../constants/common";
 import Labels from "../labels";
@@ -12,23 +11,14 @@ const renderPage = ({ tab, props }) => {
   const Component = { [TABS.LABELS]: Labels, [TABS.MILESTONES]: Milestones }[
     tab
   ];
+
   return <Component {...props} />;
 };
 
 const Index = () => {
-  const [labels, setLabels] = useState([]);
   const [activeTab, setActiveTab] = useState(TABS.LABELS);
   const [openNewLabel, setOpenNewLabel] = useState(false);
 
-  useEffect(() => {
-    fetchLabels()
-      .then((data) => {
-        setLabels(data);
-      })
-      .catch((e) => {
-        alert(e);
-      });
-  }, []);
   return (
     <main className={styles.index}>
       <Header />
@@ -40,7 +30,7 @@ const Index = () => {
         />
         {renderPage({
           tab: activeTab,
-          props: { labels, setOpenNewLabel, openNewLabel },
+          props: { setOpenNewLabel, openNewLabel },
         })}
       </Main>
     </main>
--- src/components/pages/labels/index.js
+++ src/components/pages/labels/index.js
@@ -1,7 +1,24 @@
 import NewLabelSection from "../../organisms/new-label-section";
 import IssueTable from "../../organisms/issue-table";
+import { useEffect, useState } from "react";
+import { fetchLabels } from "../../../apis/labels";
 
-const Labels = ({ openNewLabel, setOpenNewLabel, labels }) => {
+const Labels = ({ openNewLabel, setOpenNewLabel }) => {
+  const [labels, setLabels] = useState([]);
+  const setLabelsFromServer = () => {
+    fetchLabels()
+      .then((data) => {
+        setLabels(data);
+      })
+      .catch((e) => {
+        alert(e);
+      });
+  };
+
+  useEffect(() => {
+    setLabelsFromServer();
+  }, []);
+
   return (
     <>
       {openNewLabel && <NewLabelSection setOpenNewLabel={setOpenNewLabel} />}
