f14ee45fc327d5f7a25d85d5af29bf9b80811b81
parkyes90 <parkyes90@gmail.com>
2021-04-25 23:10:40 +0900

    refactor: Issue Table row 별도 컴포넌트 분리
    - 공통 스타일 믹스인

--- src/components/organisms/issue-table/index.js
+++ src/components/organisms/issue-table/index.js
@@ -1,17 +1,7 @@
 import styles from "./index.module.scss";
-import Label from "../../atoms/label";
-import ActionButton from "../../atoms/action-button";
-import { deleteLabel } from "../../../apis/labels";
-import { useContext } from "react";
-import { LabelsContext } from "../../pages/labels/context";
+import IssueTableRow from "../../molecules/issue-table-row";
 
 const IssueTable = ({ labels }) => {
-  const { setLabelsFromServer } = useContext(LabelsContext);
-  const handleDelBtnClick = (labelId) => async () => {
-    await deleteLabel(labelId);
-    setLabelsFromServer();
-  };
-
   return (
     <table className={styles.table}>
       <thead className={styles.thead}>
@@ -21,26 +11,7 @@ const IssueTable = ({ labels }) => {
       </thead>
       <tbody>
         {labels.map((label) => {
-          return (
-            <tr className={styles.tr} key={label.id}>
-              <td>
-                <Label
-                  name={label.name}
-                  color={label.color}
-                  backgroundColor={label.backgroundColor}
-                />
-              </td>
-              <td>{label.description}</td>
-              <td>
-                <div>
-                  <ActionButton>Edit</ActionButton>
-                  <ActionButton onClick={handleDelBtnClick(label.id)}>
-                    Delete
-                  </ActionButton>
-                </div>
-              </td>
-            </tr>
-          );
+          return <IssueTableRow label={label} key={label.id} />;
         })}
       </tbody>
     </table>
