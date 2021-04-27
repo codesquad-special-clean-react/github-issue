0f8831664624aa0a03c872bf017761a51e0e5fbb
kmkim <kmkim@triphos.co.kr>
2021-04-27 14:50:54 +0900

    fetch: 랜덤 color 생성 로직 추가

--- github-issue/src/components/LabelCustom.js
+++ github-issue/src/components/LabelCustom.js
@@ -11,22 +11,44 @@ const LabelCustom = ({openNewLabel}) => {
     const [color, setColor] = useState("");
     const [disabledYn, setDisabledYn] = useState(false);
 
+
     const onChangeName = ({target}) => {
         const nameVal = target.value;
 
+        if (nameVal.length > 20) return alert("최대 20자 까지 입력가능합니다.");
+
         setName(nameVal);
-        setDisabledYn(nameVal.trim().length > 0);
+        setDisabledYn(nameVal || desc || color);
     }
 
     const onChangeDesc = ({target}) => {
         const descVal = target.value;
 
         setDesc(descVal);
-        setDisabledYn(descVal.trim().length > 0);
+        setDisabledYn(descVal || name || color);
+    }
+
+    const onChangeColor = ({target}) => {
+        const colorVal = target.value;
+
+        setColor(colorVal);
+        setDisabledYn(colorVal || name || desc);
+    }
+
+    const makeRandomColor = () => {
+        const randomColor = `#${Math.round(Math.random() * 0xffffff).toString(16)}`
+
+        setColor(randomColor);
     }
 
     const onClickCreateLabel = ({target}) => {
-        console.log("click!");
+        let chkValidation = [];
+
+        if (!name) chkValidation.push("이름")
+        if (!desc) chkValidation.push("상세")
+        if (!color) chkValidation.push("색상")
+        
+        if (chkValidation.length > 0) return alert(`${chkValidation.join(" / ")} 정보를 입력해 주세요`);
     }
 
     const disabledNewLabelBtn = () => {
@@ -35,11 +57,12 @@ const LabelCustom = ({openNewLabel}) => {
         (disabledYn)
             ? $newLabelBtn.classList.remove("disabled")
             : $newLabelBtn.classList.add("disabled");
+
     }
 
     useEffect(() => {
         disabledNewLabelBtn();
-    }, [disabledYn])
+    }, [disabledYn, color])
 
 
     return (
@@ -53,7 +76,7 @@ const LabelCustom = ({openNewLabel}) => {
                 <div className="label-info">
                     <div className="input-title-style name">
                         <label>Label Name</label>
-                        <input type="text" placeholder="Label name" onChange={onChangeName}/>
+                        <input type="text" placeholder="Label name" maxLength={20} onChange={onChangeName} />
                     </div>
 
                     <div className="input-title-style desc">
@@ -64,13 +87,13 @@ const LabelCustom = ({openNewLabel}) => {
                     <div className="input-title-style color">
                         <label>Color</label>
                         <div>
-                            <button className="button-style reset-btn"></button>
-                            <input type="text" className="color-code" />
+                            <button className="button-style reset-btn" onClick={makeRandomColor}></button>
+                            <input type="text" className="color-code" id="colorCodeInput" onChange={onChangeColor} value={color}/>
                         </div>
                     </div>
 
                     <button className="button-style cancel-btn" onClick={openNewLabel}>Cancel</button>
-                    <button className="button-style green disabled" id="createLabelBtn" onClick={onClickCreateLabel} disabled={disabledYn}>
+                    <button className="button-style green disabled" id="createLabelBtn" onClick={onClickCreateLabel} >
                         Create Label
                     </button>
                 </div>
