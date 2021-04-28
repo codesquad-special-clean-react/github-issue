4ec429f16ccd11bee4240c15ce6cd1d65ea76d92
kmkim <kmkim@triphos.co.kr>
2021-04-28 16:25:19 +0900

    fetch: 라벨추가 기능 구현

--- github-issue/src/components/LabelItem.js
+++ github-issue/src/components/LabelItem.js
@@ -2,20 +2,7 @@ import "../css/LabelItem.css";
 import { useState, useEffect } from 'react';
 import {fetchLabels, deteleLabel} from "../api/LabelApi";
 
-function LabelItem({}) {
-    const [labels, setLabels] = useState("");
-    let items = "";
-
-    const getLabels = () => {
-        fetchLabels("http://localhost:3001/labels")
-            .then((data) => {
-                setLabels(data);
-            })
-            .then(() => {
-                makeLabelItems();
-            })
-    }
-
+function LabelItem({labels}) {
     const onClickDelete = async ({currentTarget}) => {
         const id = currentTarget.closest("li.label-item").getAttribute("data-id");
 
@@ -23,30 +10,27 @@ function LabelItem({}) {
         await getLabels();
     }
 
-    const makeLabelItems = () => {
-        if (labels.length <= 0) return null;
-
-        items = labels.map( ({ id, name, desc, color, deleteBool }) => {
-            return (
-                <li className="label-item" key={id} data-id={id}>
-                    <div className="label-name">
-                        <div style={{backgroundColor: color}}>{name}</div>
-                    </div>
-                    <div className="description">{desc}</div>
-                    <div className="label-buttons">
-                        <div className="edit">Edit</div>
-                        <div className="delete" onClick={onClickDelete}>Delete</div>
-                    </div>
-                </li>
-            )
-        });
+    const getLabels = () => {
+        fetchLabels("http://localhost:3001/labels")
+        .then((data) => {
+            labels = data;
+        })
     }
 
-    makeLabelItems();
-
-    useEffect( ()=> {
-        getLabels();
-    }, []);
+    let items = labels.map( ({ id, name, desc, color }) => {
+        return (
+            <li className="label-item" key={id} data-id={id}>
+                <div className="label-name">
+                    <div style={{backgroundColor: color}}>{name}</div>
+                </div>
+                <div className="description">{desc}</div>
+                <div className="label-buttons">
+                    <div className="edit">Edit</div>
+                    <div className="delete" onClick={onClickDelete}>Delete</div>
+                </div>
+            </li>
+        )
+    });
 
     return (
         <>
--- github-issue/src/components/LabelList.js
+++ github-issue/src/components/LabelList.js
@@ -5,27 +5,26 @@ import {fetchLabels} from "../api/LabelApi";
 
 function LabelList() {
     const [labels, setLabels] = useState([]);
+    const [labelsLength, setLabelsLength] = useState(0);
 
     const getLabels = () => {
         fetchLabels("http://localhost:3001/labels")
             .then((data) => {
                 setLabels(data);
-            })
-            .then(() => {
-                // makeLabelItems();
+                setLabelsLength(data.length);
             })
     }
 
     useEffect( ()=> {
         getLabels();
-    }, []);
+    }, [labels]);
 
     return (
         <div className="label-list-container">
             <ul className="label-list">
                 <li className="label-title">
                     <div>
-                        <b>{labels.length} labels</b>
+                        <b>{labelsLength} labels</b>
                     </div>
                 </li>
                 <LabelItem labels={labels}/>
