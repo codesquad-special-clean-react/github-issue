3dc145e2e6eff1962554d8ac5408b5a05d0f37e1
parkyes90 <parkyes90@gmail.com>
2021-05-02 19:32:23 +0900

    feat: 마일스톤 테이블 렌더링

--- src/components/organisms/milestones/milestone-table/index.js
+++ src/components/organisms/milestones/milestone-table/index.js
@@ -1,21 +1,78 @@
 import styles from "./index.module.scss";
+import { useContext } from "react";
+import { MilestonesContext } from "../../../pages/milestones/context";
 
 const MilestoneTable = () => {
+  const {
+    state: { milestones },
+  } = useContext(MilestonesContext);
+  const getIssueCount = (key) => {
+    return milestones.reduce((acc, milestone) => {
+      return acc + milestone[key];
+    }, 0);
+  };
+  const openedIssueCount = getIssueCount("openedIssueCount");
+  const closedIssueCount = getIssueCount("closedIssueCount");
   return (
     <table className={styles.table}>
       <thead className={styles.thead}>
         <tr className={styles.tr}>
           <th colSpan={4}>
-            <span>2 Open</span>
-            <span>0 Closed</span>
+            <span>{openedIssueCount} Open</span>
+            <span>{closedIssueCount} Closed</span>
           </th>
         </tr>
       </thead>
       <tbody>
-        <tr className={styles.tr}>
-          <td>tes</td>
-          <td>tes</td>
-        </tr>
+        {milestones.map(
+          ({
+            id,
+            title,
+            dueDate,
+            description,
+            openedIssueCount,
+            closedIssueCount,
+          }) => {
+            const progressPercent = (
+              (closedIssueCount / (closedIssueCount + openedIssueCount)) *
+              100
+            ).toFixed(0);
+
+            return (
+              <tr className={styles.tr} key={id}>
+                <td>
+                  <div className={styles["container"]}>
+                    <div className={styles.title}>{title}</div>
+                    <div>Due by {dueDate}</div>
+                    <div>{description}</div>
+                  </div>
+                </td>
+                <td>
+                  <div className={styles["container"]}>
+                    <div>
+                      <div className={styles["progress-bar"]}>
+                        <div
+                          className={styles["progress"]}
+                          style={{ width: `${progressPercent}%` }}
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <span>{progressPercent}% complete</span>
+                      <span>{openedIssueCount} open</span>
+                      <span>{closedIssueCount} closed</span>
+                    </div>
+                    <div className={styles["button-group"]}>
+                      <button className={styles.edit}>Edit</button>
+                      <button className={styles.edit}>Close</button>
+                      <button className={styles.delete}>Delete</button>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            );
+          }
+        )}
       </tbody>
     </table>
   );
