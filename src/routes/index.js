9c2ce91b2ae21a135dcd913e7bf72c078369de6a
parkyes90 <parkyes90@gmail.com>
2021-05-02 19:39:45 +0900

    feat: 마일스톤 추가 & 수정 route 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 import { PATHS } from "../constants/paths";
 import Labels from "../components/pages/labels";
 import Milestones from "../components/pages/milestones";
+import MilestoneForm from "../components/organisms/milestones/milestone-form";
 
 export const ROUTES = [
   { path: PATHS.LABELS_PATH, component: Labels },
+  { path: PATHS.NEW_MILESTONE_PATH, component: MilestoneForm },
+  { path: PATHS.EDIT_MILESTONE_PATH, component: MilestoneForm },
   { path: PATHS.MILESTONES_PATH, component: Milestones },
 ];
