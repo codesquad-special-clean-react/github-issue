200cc66434c46901e81fed1be64b68143727f5b8
kmkim <kkgg7527@gmail.com>
2021-05-17 07:23:05 +0900

    update: state 사용 로직 reducer로 변경

--- github-issue/src/components/milestone/NewMilestone.js
+++ github-issue/src/components/milestone/NewMilestone.js
@@ -2,37 +2,60 @@ import CommentTitle from "../milestone/CommentTitle";
 import TitleInput from "../templates/TitleInput";
 import TitleTextarea from "../templates/TitleTextarea";
 import Button from "../templates/Button";
-import React, {useState} from 'react';
-import styled from 'styled-components';
+import React, {useReducer} from 'react';
 import {addLabels} from "../../api/LabelApi";
 import {Link} from "react-router-dom";
+import {milestoneUrl} from "../../api/ApiUrl";
+import styled from 'styled-components';
+import {DateFormat} from "../../util/DateFormat";
 
 const NewMilestone = () => {
-	const [newMilestoneInfo, setNewMilestoneInfo] = useState({
+
+	const initialState = {
 		title: "",
 		dueDate: "",
 		desc: "",
 		activeType: "open",
 		issueCnt: 0,
 		closedIssueCnt: 0,
-	});
+	}
 
-	const onClickCreate = () => {
-		const url = "http://localhost:3001/milestones";
+	const reducer = (state, action) => {
+		switch (action.type) {
+			case "reset": return initialState;
+			case 'updateTitle': return {...state, title: action.title};
+			case 'updateDueDate': return {...state, dueDate: action.dueDate};
+			case 'updateDesc': return {...state, desc: action.desc};
+		}
+	}
 
-		addLabels(url, newMilestoneInfo);
+	const [state, dispatch] = useReducer(reducer, initialState);
+
+	const validateDate = (date) => {
+		const result = DateFormat(date);
+
+		if (result === "break") return;
+
+		dispatch({ type: "updateDueDate", dueDate: result });
 	};
 
-	const onChangeTitle = ({target: {value: newMilestoneTitle}}) => setNewMilestoneInfo({...newMilestoneInfo, title: newMilestoneTitle });
-	const onChangeDueDate = ({target: {value: newMilestoneDueDate}}) => setNewMilestoneInfo({...newMilestoneInfo, dueDate: newMilestoneDueDate});
-	const onChangeDesc = ({target: {value: newMilestoneDesc}}) => setNewMilestoneInfo({...newMilestoneInfo, desc: newMilestoneDesc});
+	const onChangeTitle = event => dispatch({ type: "updateTitle", title: event.target.value });
+	const onChangeDueDate = event => validateDate( event.target.value );
+	const onChangeDesc = event => dispatch({ type: "updateDesc", desc: event.target.value });
+
+	const chkAddValidate = () => {
+
+		addLabels(milestoneUrl, state);
+	};
+
+	const onClickCreate = () => chkAddValidate();
 
 	return (
 		<>
 			<CommentTitle/>
 			<Contents>
 				<TitleInput label="Title" placehlder="Title" onChange={onChangeTitle}/>
-				<TitleInput label="Due date (optional)" placehlder="연도. 월. 일" onChange={onChangeDueDate}/>
+				<TitleInput label="Due date (optional)" placehlder="연도. 월. 일" onChange={onChangeDueDate} value={state.dueDate}/>
 				<TitleTextarea label="Description (optional)" onChange={onChangeDesc}/>
 			</Contents>
 			<Link to="/"><Button type="green" onClick={onClickCreate}>Create milestone</Button></Link>
